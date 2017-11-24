e4e55773067786063700cdf896574b0c413ecfa8
root <root@ubuntu>
2017-11-24 23:14:53 +0000

    3

--- SELE/slave.c
+++ SELE/slave.c
@@ -10,15 +10,41 @@ void init_usart(void) {
 	UCSR0A = (1 << U2X0); // Double speed
 
 	// Definir formato da trama
-	UCSR0C = (3 << UCSZ00) // 8 data bits
-	| (0 << UPM00) // no parity
-			| (0 << USBS0); // 1 stop bit
+	UCSR0C = (5 << UCSZ00) // 9 data bits
+			| (0 << UPM00) // no parity
+			| (0 << USBS0) // 1 stop bit
+			| (0 << UMSEL00)
+			| (0 << UMSEL01); //comunicacao assincrona
 
-	// Ativar apenas o emissor
+	// Ativar emissao e rececao
 	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
 }
 
+/*
+ * Para enviar um byte basta escreve-lo
+ * no registo UDR0, verificando antes se
+ * este está disponível (bit UDRE0 de UCSR0A)
+ *
+ */
+void send_buffer(uint8_t buffer) {
+	// Espera que UDR0 esteja vazio
+	while ((UCSR0A & (1 << UDRE0)) == 0)
+		;
+
+	//ativar 9 bit para endereco
+	UCSR0B = (1 << RXB80);
+
+	UDR0 = buffer; // Envia para a porta serie
+}
+
 int main(int argc, char **argv) {
+	//inteiro de 8bits
+	uint8_t buffer = 0;
+
+	init_usart();
+
+
+
 	return 0;
 }
 
