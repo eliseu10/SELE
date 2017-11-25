810b944a4c5e3fdf815e44f0afdb8df996c984c6
Hélio Puga <pugahelio@gmail.com>
2017-11-25 01:53:03 +0000

    maquinas de estados

--- SELE/Release/subdir.mk
+++ SELE/Release/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../slave.c 
+../Slave.c 
 
 OBJS += \
-./slave.o 
+./Slave.o 
 
 C_DEPS += \
-./slave.d 
+./Slave.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
