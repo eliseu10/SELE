bebe93227df14716bddf92ab673fd8672cbd1c79
root <root@ubuntu>
2017-11-27 16:13:58 +0000

    alteracao

--- SELE/Slave.c
+++ SELE/Slave.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
 
+#define F_CPU 16000000UL
 #define	baud 57600  // baud rate
-#define baudgen ((F_CPU/(8*baud))-1)  //baud divider
+#define baudgen ((F_CPU/(16*baud))-1)  //baud divider
 #define UPCOUNT 1
 #define DOWNCOUNT 2
 #define ERRORCOUNT 9999
@@ -20,7 +21,7 @@
 #define SENDCOUNT 1
 #define RECEIVESTATE 2
 
-#define SLAVEADDR 0xAA
+#define SLAVEADDR 0x01
 #define GREENCODE 0x01
 #define REDCODE 0x02
 
@@ -44,10 +45,10 @@ void init_usart(void) {
 	// Definir baudrate
 	UBRR0H = (uint8_t) (baudgen >> 8);
 	UBRR0L = (uint8_t) baudgen;
-	UCSR0A = (1 << U2X0); // Double speed
+	//UCSR0A = (1 << U2X0); // Double speed
 
 	// Definir formato da trama
-	UCSR0C = (5 << UCSZ00) // 9 data bits
+	UCSR0C = (7 << UCSZ00) // 9 data bits
 			| (0 << UPM00) // no parity
 			| (0 << USBS0) // 1 stop bit
 			| (0 << UMSEL00)
